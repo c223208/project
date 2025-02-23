fee1a65f2a26b1c678a3932f044c860060e8d065
c223208 <c223208@ed.nagoya-cu.ac.jp>
2025-02-23 17:02:07 +0900

    app.py

--- app.py
+++ app.py
@@ -1,26 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 
+# ==================================================
+# インスタンス生成
+# ==================================================
 app = Flask(__name__)
+import os
+# 乱数を設定
+app.config['SECRET_KEY'] = os.urandom(24)
+
+# ==================================================
+# ルーティング
+# ==================================================
+from forms import InputForm
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/home_save')
-def home_save():
-    return render_template('home_save.html')
+@app.route('/input_home')
+def input_home():
+    return render_template('input_home.html')
 
 @app.route('/calendar')
 def calendar():
     return render_template('calendar.html')
 
-@app.route('/calendar_medicine')
-def calendar_medicine():
-    return render_template('calendar_medicine.html')
+@app.route('/input_calendar')
+def input_calendar():
+    return render_template('input_calendar.html')
 
 @app.route('/folder')
 def folder():
     return render_template('folder.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
